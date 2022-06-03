8f29eeb595bf4922d45c8d348a234c2870101715
Varun221 <yerram.varun@gmail.com>
2022-06-03 17:28:09 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+function printLetterByLetter(destination, message, speed){
+    var i = 0;
+    var interval = setInterval(function(){
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+            clearInterval(interval);
+        }
+    }, speed);
+}
